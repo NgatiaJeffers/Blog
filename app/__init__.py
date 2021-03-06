35f4a99adfaf890f0e9534f72f17e607e1f16d53
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-07 02:02:48 +0300

    Initialize login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# Initializing Login manager
+login_manager = LoginManager()
+login_manager.session_protection = 'storng'
+login_manager.login_view = 'auth_login'
 
 # Initializing Extensions
 bootstrap = Bootstrap()
@@ -21,5 +27,8 @@ def create_app(config_name):
     # Registering the BLUEPRINT
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
 
     return app
